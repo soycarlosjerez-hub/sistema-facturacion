f563c77e036a7e4ba5959c8e8e87ea58dfc40aa9
Carlos Jerez <jcjerez@gmail.com>
2026-08-18 14:15:11 -0400

    Fixed

--- update_kds.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\DB;
-
-DB::table('productos')->whereNull('incluir_kds')->update(['incluir_kds' => true]);
-DB::table('productos')->where('incluir_kds', 0)->update(['incluir_kds' => 1]);
-
-echo "Productos actualizados a incluir_kds = true\n";
