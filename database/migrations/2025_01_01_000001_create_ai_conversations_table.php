73807a6af9f69cd821b063abf23db41db287a628
Carlos Jerez <jcjerez@gmail.com>
2026-08-24 18:22:06 -0400

    Fixed

--- database/migrations/2025_01_01_000001_create_ai_conversations_table.php
+++ database/migrations/2025_01_01_000001_create_ai_conversations_table.php
@@ -8,6 +8,9 @@ return new class extends Migration
 {
     public function up(): void
     {
+        if (Schema::hasTable('ai_conversations')) {
+            return;
+        }
         Schema::create('ai_conversations', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('tenant_id');
